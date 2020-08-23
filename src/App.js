6f96ffc79cc042489b8e4f23bb941447f4bf381a
vfinchuk <finchukvladimir@gmail.com>
2020-08-23 23:01:46 +0300

    Add HomePage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
   return (
     <div>
-      <h1>Initial</h1>
+      <HomePage />
     </div>
   )
 }
